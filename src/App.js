5c304d09b2517087b00764e25a9fc9b5d2357ffb
Paolo Venditti <Paolo@MacBook-Pro-di-Paolo.local>
2024-03-26 13:16:36 +0100

    fine seconda lezione

--- src/App.js
+++ src/App.js
@@ -1,39 +1,94 @@
-import './App.scss';
+import "./App.scss";
+import HomeIcon from "@mui/icons-material/Home";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import MarkAsUnreadIcon from "@mui/icons-material/MarkAsUnread";
+import { useState } from "react";
+import Logo from "./assets/images/icona-cibando.png";
 
 function App() {
+  const [pippo, setPippo] = useState("ciao");
+  const [interruttore, setInterruttore] = useState(false);
+
+  function cambiaValore() {
+    if (pippo=== 'ciao'){
+      setPippo('buongiorno');
+  }else {
+    setPippo('ciao');
+  }
+
+  }
+
   return (
-  <>
-        <header>
-        <nav class="navbar navbar-expand-lg bg-red navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Cibando</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <>
+      <header>
+        <nav className="navbar navbar-expand-lg bg-red navbar-dark">
+          <div className="container-fluid">
+            <img src={Logo} className="icona-cibando"/>
+            <a className="navbar-brand" href="#">
+              Cibando
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    <HomeIcon>Filled</HomeIcon>Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <MenuBookIcon>Filled</MenuBookIcon>Ricette
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <MarkAsUnreadIcon>Filled</MarkAsUnreadIcon>Contatti
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="container-titolo">
+        <h2> App di esempio</h2>
+        <p className="paragrafo">Ecco il testo iniziale</p>
+        <h3>{pippo}</h3>
+
+        <button className="btn btn-primary" onClick={cambiaValore}>
+          Cambia
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Ricette</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contatti</a>
-            </li>
-          </ul>
+        <div>
+          {interruttore ? (
+            <div>
+              <h2>Luce Accesa</h2>
+            </div>
+          ) : (
+            <div>
+              <h2>Luce Spenta</h2>
+            </div>
+          )}
+          <button
+            style={{ background: interruttore ? "red" : "green" }}
+            className="btn btn-primary"
+            onClick={() => setInterruttore(!interruttore)}
+          >
+            {interruttore ? "spegni" : "accendi"}
+          </button>
         </div>
       </div>
-    </nav>
-    </header>
-    <div class ="container-titolo">
-      <h2> App di esempio</h2>
-      <p class="paragrafo">Ecco il testo iniziale</p>
-    </div>
-  </>
+    </>
   );
- 
 }
-
 
 export default App;
