6051de1ec90ffa3ef070a81bb0599e07bebb6e3a
Paolo Venditti <Paolo@MACBOOKPRO-5125.station>
2024-05-02 13:09:45 +0200

    lezione undici

--- src/api/recipeApi.js
+++ src/api/recipeApi.js
@@ -1,20 +1,33 @@
-import { error } from "jquery";
 import RECIPES from "../mocks/recipes-mock";
-
-async function getRecipes() {
+const apiBaseUrl = '/api/recipes'
+/* async function getRecipes() {
   try {
     const response = await RECIPES;
     return response;
   } catch (error) {
     console.log(error);
   }
+} */
+
+async function getRecipes() {
+  try{
+    const response = await fetch(apiBaseUrl + '/trovaTutto');
+    if(response.ok) {
+      const data =await response.json();
+      return data
+    }else {
+      throw new Error ('Errore nella richiesta al server !') // SE LA RICHIESTA è STATA FATTA MALE
+    }
+  } catch (error) {
+    console.log(error)
+}
 }
 
 const getRecipe = async (id) => {
-  try{
-      const response = await RECIPES.find(iteam => iteam._id === id)
-      return (response);
-    } catch (error) {
+  try {
+      const response = await RECIPES.find(ricetta => ricetta._id ===  id);
+      return response;
+  } catch (error) {
       console.log(error)
   }
 }
